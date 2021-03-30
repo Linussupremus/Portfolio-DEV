386ce876c5921f32dd6ca8f4cc32ebbbd9c7f201
Linas <linus.supremus@gmail.com>
2021-03-31 00:23:10 +0300

    commit

---
+++ js/mainHome.js
@@ -0,0 +1,29 @@
+// IMPORT
+/* header */
+/* hero */
+/* clients */
+/* services */
+/* about company */
+/* hire us */
+/* achievements */
+/* promo video */
+/* case studies */
+/* testimonials */
+/* blog */
+/* contacts */
+/* footer */
+
+// FUNKCIJU PANAUDOJIMAS
+/* header */
+/* hero */
+/* clients */
+/* services */
+/* about company */
+/* hire us */
+/* achievements */
+/* promo video */
+/* case studies */
+/* testimonials */
+/* blog */
+/* contacts */
+/* footer */
